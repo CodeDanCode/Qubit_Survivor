dd053910c13e68c0991b40503a97409212184091
Daniel Leach <dannylcas@gmail.com>
2022-11-07 11:09:17 -0500

    corrected main.py file

---
+++ main/main.py
@@ -0,0 +1,47 @@
+import pygame, sys
+from level import *
+from settings import *
+from menu import *
+from qiskit import IBMQ
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        # self.level = Level()
+        self.connect()
+        self.menu = Menu()
+
+    def connect(self):
+        #
+        IBMQ.save_account(TOKEN)
+
+        IBMQ.load_account() # opens account to get access to backends
+        provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
+        backend = provider.get_backend('ibmq_manila') # random backend
+
+        # pretends to create the circuit 
+        qoutput = "Creating circuit..."
+        # print(qoutput)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            dt = self.clock.tick() /1000
+            # self.level.run(dt)
+            self.menu.run(dt)
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Main()
+    game.run()
